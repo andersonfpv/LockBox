d9fb1126da631ea6f795ba2c1ef97e96c36e4b90
A12 <A12@NOTEA1208.santuario.local>
2025-07-15 20:23:44 -0300

    Melhorando as rotas

---
+++ App/Controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class LoginController {
+    public function index(){
+        echo "logincontroller.index";
+    }
+
+
+    public function login(){
+        echo "logincontroller.login";
+    }
+}
--- routes.php
+++ routes.php
@@ -1,5 +1,20 @@
 <?php
 
+use Core\Route;
+
+use App\Controllers\IndexController;
+
+use App\Controllers\LoginController;
+
+(new Route())
+
+->get('/', IndexController::class)
+
+->get('/login', [LoginController::class], 'index')
+->post('/login', [LoginController::class], 'index')
+
+->run();
+
 $controller = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
     
 if(!$controller) $controller = 'index';
