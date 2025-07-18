692978e9abffcb143eecdcb0e26d9d7ec6d9de4f
A12 <A12@NOTEA1208.santuario.local>
2025-07-17 22:34:03 -0300

    Ajustes login e registrar

--- views/login.view.php
+++ views/login.view.php
@@ -11,20 +11,42 @@
     <div class="bg-white hero mr-40 min-h-screen text-black">
         <div class="hero-content -mt-20">
             <form method="post" action="/login">
-                <?php 
-                    $validacoes = flash()->get('validacoes'); 
+                <?php
+                $validacoes = flash()->get('validacoes');
+                ?>
+                <?php
+                $success = flash()->get('mensagem');
                 ?>
                 <div class="card">
                     <div class="card-body">
                         <div class="card-title">Faça o seu login</div>
+                        <?php if ($success): ?>
+                            <div role="alert" class="alert shadow-lg">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    class="stroke-info h-6 w-6 shrink-0">
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
+                                <div>
+
+                                    <div class="text-xs"><?= $success ?> </div>
+                                </div>
+                            </div>
+                        <?php endif; ?>
                         <label class="form-control">
                             <div class="label">
                                 <span class="label-text text-black">E-mail</span>
                             </div>
-                            <input type="email" name="email" 
-                            class="input input-bordered w-full max-w-xs bg-white" 
-                            value="<?=old('email')?>" />
-                            <?php if(isset($validacoes['email'])) : ?>
+                            <input type="email" name="email"
+                                class="input input-bordered w-full max-w-xs bg-white"
+                                value="<?= old('email') ?>" />
+                            <?php if (isset($validacoes['email'])) : ?>
                                 <div class="label text-xs text-error"><?= $validacoes['email'][0] ?></div>
                             <?php endif; ?>
                         </label>
@@ -32,10 +54,10 @@
                             <div class="label">
                                 <span class="label-text text-black">Senha</span>
                             </div>
-                            <input type="password" name="senha" 
-                            class="input input-bordered w-full max-w-xs bg-white" 
-                            value="<?=old('senha')?>" />
-                            <?php if(isset($validacoes['senha'])) : ?>
+                            <input type="password" name="senha"
+                                class="input input-bordered w-full max-w-xs bg-white"
+                                value="<?= old('senha') ?>" />
+                            <?php if (isset($validacoes['senha'])) : ?>
                                 <div class="label text-xs text-error"><?= $validacoes['senha'][0] ?></div>
                             <?php endif; ?>
                         </label>
--- views/registrar.view.php
+++ views/registrar.view.php
@@ -11,6 +11,9 @@
     <div class="bg-white hero mr-40 min-h-screen text-black">
         <div class="hero-content -mt-20">
             <form method="post" action="/registrar">
+                <?php 
+                    $validacoes = flash()->get('validacoes'); 
+                ?>
                 <div class="card">
                     <div class="card-body">
                         <div class="card-title">Crie a sua conta</div>
@@ -18,25 +21,40 @@
                             <div class="label">
                                 <span class="label-text text-black">Nome</span>
                             </div>
-                            <input type="text" class="input input-bordered w-full max-w-xs bg-white" />
+                            <input type="text" name="nome" 
+                            class="input input-bordered w-full max-w-xs bg-white" 
+                            value="<?=old('nome')?>" />
+                            <?php if(isset($validacoes['nome'])) : ?>
+                                <div class="label text-xs text-error"><?= $validacoes['nome'][0] ?></div>
+                            <?php endif; ?>
                         </label>
                         <label class="form-control">
                             <div class="label">
                                 <span class="label-text text-black">E-mail</span>
                             </div>
-                            <input type="email" class="input input-bordered w-full max-w-xs bg-white" />
+                            <input type="email" name="email" 
+                            class="input input-bordered w-full max-w-xs bg-white" 
+                            value="<?=old('email')?>"/>
+                            <?php if(isset($validacoes['email'])) : ?>
+                                <div class="label text-xs text-error"><?= $validacoes['email'][0] ?></div>
+                            <?php endif; ?>
                         </label>
                         <label class="form-control">
                             <div class="label">
                                 <span class="label-text text-black">Confirme seu e-mail</span>
                             </div>
-                            <input type="email" class="input input-bordered w-full max-w-xs bg-white" />
+                            <input type="email" name="email_confirmacao" class="input input-bordered w-full max-w-xs bg-white" />
                         </label>
                         <label class="form-control">
                             <div class="label">
                                 <span class="label-text text-black">Senha</span>
                             </div>
-                            <input type="password" class="input input-bordered w-full max-w-xs bg-white" />
+                            <input type="password" name="senha" 
+                            class="input input-bordered w-full max-w-xs bg-white" 
+                            value="<?=old('senha')?>" />
+                            <?php if(isset($validacoes['senha'])) : ?>
+                                <div class="label text-xs text-error"><?= $validacoes['senha'][0] ?></div>
+                            <?php endif; ?>
                         </label>
                         <div class="card-actions">
                             <button class="btn btn-primary btn-block">Registrar</button>
